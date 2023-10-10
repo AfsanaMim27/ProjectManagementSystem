f31d53cf125bbc4df5a20cf66e6f41bce627a3c4
Ehsan Kabir <2001027@iot.bdu.ac.bd>
2023-10-10 10:57:40 +0600

    Fix issue: Pagination is not working in Project list page

--- src/app/api/projects/route.js
+++ src/app/api/projects/route.js
@@ -2,8 +2,9 @@ import { connectToDatabase, getPageNumberAndSize } from "@/app/api/services";
 
 export async function GET(request, response) {
     const { page, size } = getPageNumberAndSize(request);
-    let dbConnection = await connectToDatabase(); 
+    let dbConnection = await connectToDatabase();
     const [rows, fields] = await dbConnection.execute("SELECT p.*, CONCAT(c.FirstName, ' ', c.LastName) AS ProjectManagerName FROM projects AS p INNER JOIN contacts AS c ON p.ProjectManager = c.ContactId LIMIT " + size + " OFFSET " + ((page - 1) * size));
-    const [projectCounts, projectCountsFields] = await dbConnection.execute('SELECT COUNT(*) AS TotalProjects');
-    return Response.json({ records: rows, pagination: {totalPages: Math.ceil(parseInt(projectCounts[0].TotalProjects)/size), totalItems: projectCounts[0].TotalProjects} });    
+    const [projectCounts, projectCountsFields] = await dbConnection.execute('SELECT COUNT(*) AS TotalProjects FROM Projects');
+    console.log(projectCounts);
+    return Response.json({ records: rows, pagination: { totalPages: Math.ceil(parseInt(projectCounts[0].TotalProjects) / size), totalItems: projectCounts[0].TotalProjects } });
 }
