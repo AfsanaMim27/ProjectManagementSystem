5393d5dbac623fa9f0277e9db6f149857f195cbc
Ehsan Kabir <2001027@iot.bdu.ac.bd>
2023-10-28 23:07:46 +0600

    Added TasksController, ProjectsController and Separate files for code reusability

--- PMS_API/src/main/java/com/PMS_API/ContactsController.java
+++ PMS_API/src/main/java/com/PMS_API/ContactsController.java
@@ -1,25 +1,12 @@
 package com.PMS_API;
 
-//import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-//import jakarta.servlet.ServletContext;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.sql.*;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Date;
-import java.io.File;
-import java.io.FileWriter;
 
 @RestController
 @RequestMapping("/api/contacts")
@@ -29,7 +16,6 @@ public class ContactsController extends DatabaseConnection implements ErrorLoggi
     public List<Contact> contactList() {
         List<Contact> contactList = new ArrayList<>();
         try {
-            logErrorFile("/api/contacts/", "request started");
             DatabaseConnection dc = new DatabaseConnection();
             Statement statement = dc.DbConnection.createStatement();
             ResultSet resultSet = statement.executeQuery("SELECT * FROM Contacts WHERE Trashed = 0");
@@ -71,6 +57,9 @@ public class ContactsController extends DatabaseConnection implements ErrorLoggi
                 dc.DbConnection.close();
                 return true;
             } else {
+                resultSet.close();
+                statement.close();
+                dc.DbConnection.close();
                 logErrorFile("/api/contacts/delete/" + id, "Contact not found.");
             }
         } catch (SQLException e) {
@@ -79,56 +68,12 @@ public class ContactsController extends DatabaseConnection implements ErrorLoggi
         return false;
     }
 
-    @GetMapping("/error_logs")
-    public String error_logs() {
-        try {
-            Path filePath = Paths.get(ErrorLogging.GetErrorLogFilePath());
-            String fileContent = Files.readString(filePath);
-            return fileContent;
-        } catch (IOException e) {
-            System.out.println("An unexpected error is occurred when errors are retrieved.");
-            return "No errors found";
-        }
-    }
-
     public void logErrorFile(String url, String errorMessage) {
-        try {
-            String filePath = ErrorLogging.GetErrorLogFilePath();
-            File logFile = new File(filePath);
-            if (!(logFile.exists())) {
-                boolean isFileCreated = logFile.createNewFile();
-                System.out.println(isFileCreated ? (filePath + " file is created successfully.") : (filePath + " file create fails."));
-            }
-            StringBuilder sb = new StringBuilder();
-            sb.append("{");
-            sb.append("\nUrl: ").append(url);
-            sb.append("\nError: ").append(errorMessage);
-            sb.append("\n},\n");
-            Files.write(Paths.get(filePath), sb.toString().getBytes(), StandardOpenOption.APPEND);
-        } catch (IOException exception) {
-            System.out.println("An unexpected error is occurred when error is logged.");
-        }
+        ErrorLogging.StoreErrorInFile(url, errorMessage);
     }
 
     public void logErrorDB(String url, String errorMessage) {
-        try {
-            DatabaseConnection dc = new DatabaseConnection();
-            Statement statement = dc.DbConnection.createStatement();
-            Date currentDateTime = new Date();
-            SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd hh:mm");
-            String currentDateTimeString = formatter.format(currentDateTime);
-            try {
-                statement.executeUpdate("INSERT INTO ErrorLogs (CreatedDateTime, Url, ErrorMessage) VALUES ('"
-                        + currentDateTimeString + "', '" + url + "', '" + errorMessage + "')");
-            } catch (SQLException e) {
-                System.out.println(
-                        "Storing log record in database fails. Url: " + url + ", Error message: " + e.getMessage());
-            }
-            statement.close();
-            dc.DbConnection.close();
-        } catch (Exception e) {
-            System.out.println("Creating log fails. Url: " + url + ", Error message: " + e.getMessage());
-        }
+        ErrorLogging.StoreErrorInDb(url, errorMessage);
     }
 }
 
@@ -142,30 +87,3 @@ class Contact {
     public String Email;
 }
 
-interface ErrorLogging {
-    public static String GetErrorLogFilePath() {
-        String rootPath = "C:/ProjectManagementSystem/PMS_API/";
-        String filePath = rootPath + "ErrorLog.txt";
-        return filePath;
-    }
-
-    public void logErrorFile(String url, String errorMessage);
-
-    public void logErrorDB(String url, String errorMessage);
-}
-
-class DatabaseConnection {
-    public Connection DbConnection;
-
-    DatabaseConnection() {
-        try {
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            DbConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/projecttrackingsystem", "root",
-                    "Pass5577!");
-        } catch (ClassNotFoundException exception) {
-            System.out.println("JDBC Driver is not found.");
-        } catch (SQLException exception) {
-            System.out.println("Database connection fails.");
-        }
-    }
-}
---
+++ PMS_API/src/main/java/com/PMS_API/ProjectsController.java
@@ -0,0 +1,86 @@
+package com.PMS_API;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/projects")
+public class ProjectsController extends DatabaseConnection implements ErrorLogging{
+    @GetMapping("/list")
+    public List<Project>projectList(){
+        List<Project> projectList = new ArrayList<>();
+        try{
+            DatabaseConnection dc = new DatabaseConnection();
+            Statement statement = dc.DbConnection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT p.*, CONCAT(c.FirstName, ' ', c.LastName) AS ProjectManagerName FROM projects AS p INNER JOIN contacts AS c ON p.ProjectManager = c.ContactId where p.Trashed = 0");
+            while (resultSet.next()) {
+                Project project = new Project();
+                project.ProjectId = resultSet.getInt("ProjectId");
+                project.Title = resultSet.getString("Title");
+                project.ShortDescription = resultSet.getString("ShortDescription");
+                project.StartDate = resultSet.getString("StartDate");
+                project.EndDate = resultSet.getString("EndDate");
+                project.DueDate = resultSet.getString("DueDate");
+                project.ProjectManager = resultSet.getString("ProjectManagerName");
+                projectList.add(project);
+        }
+        resultSet.close();
+            statement.close();
+            dc.DbConnection.close();
+        } catch (SQLException e) {
+            logErrorFile("/api/projects/", e.getMessage());
+        }
+        return projectList;
+    }
+    @GetMapping("/delete/{id}")
+    public Boolean deleteProject(@PathVariable("id") int id) {
+        try {
+            DatabaseConnection dc = new DatabaseConnection();
+            Statement statement = dc.DbConnection.createStatement();
+            Boolean isProjectFound = false;
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM Projects WHERE ProjectId = " + id);
+            while (resultSet.next()) {
+                int projectId = resultSet.getInt("ProjectId");
+                isProjectFound = projectId == id;
+            }
+            if (isProjectFound) {
+                statement.executeUpdate("UPDATE Projects SET Trashed = 1 WHERE ProjectId = " + id);
+                resultSet.close();
+                statement.close();
+                dc.DbConnection.close();
+                return true;
+            } else {
+                resultSet.close();
+                statement.close();
+                dc.DbConnection.close();
+                logErrorFile("/api/projects/delete/" + id, "Project not found.");
+            }
+        } catch (SQLException e) {
+            logErrorFile("/api/projects/delete/" + id, e.getMessage());
+        }
+        return false;
+    }
+
+    public void logErrorFile(String url, String errorMessage) {
+        ErrorLogging.StoreErrorInFile(url, errorMessage);
+    }
+
+    public void logErrorDB(String url, String errorMessage) {
+        ErrorLogging.StoreErrorInDb(url, errorMessage);
+    }
+}
+
+class Project {
+    public int ProjectId;
+    public String Title;
+    public String ShortDescription;
+    public String StartDate;
+    public String EndDate;
+    public String DueDate;
+    public String ProjectManager;
+}
