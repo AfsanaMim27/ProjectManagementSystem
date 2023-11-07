f59c6702e337e53b5f48e35636c7e6baca222709
AfsanaMim27 <2001027@iot.bdu.ac.bd>
2023-11-07 10:39:24 +0600

    Add post method to projects

--- PMS_API/src/main/java/com/PMS_API/ProjectsController.java
+++ PMS_API/src/main/java/com/PMS_API/ProjectsController.java
@@ -1,17 +1,23 @@
 package com.PMS_API;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/projects")
+@RequestMapping("/api")
 public class ProjectsController extends DatabaseConnection implements ErrorLogging {
-    @GetMapping("/list")
+    @GetMapping("/projects")
     public List<Project> projectList() {
         List<Project> projectList = new ArrayList<>();
         try {
@@ -34,12 +40,29 @@ public class ProjectsController extends DatabaseConnection implements ErrorLoggi
             statement.close();
             dc.DbConnection.close();
         } catch (SQLException e) {
-            logErrorFile("/api/projects/", e.getMessage());
+            logErrorFile("GET: /api/projects", e.getMessage());
         }
         return projectList;
     }
 
-    @GetMapping("/delete/{id}")
+    @PostMapping("/projects")
+    Project createProject(@RequestBody Project newProject) {
+        String insertQuery = "INSERT INTO Projects (Trashed, ProjectStatus, Title, ShortDescription, StartDate, DueDate, ProjectManager) VALUES (0, 2, '"
+                + newProject.Title + "', '" + newProject.ShortDescription + "', '" + newProject.StartDate + "', '"
+                + newProject.DueDate + "', " + newProject.ProjectManager + ")";
+        try {
+            DatabaseConnection dc = new DatabaseConnection();
+            Statement statement = dc.DbConnection.createStatement();
+            int result = statement.executeUpdate(insertQuery);
+            newProject.ProjectId = result;
+            return newProject;
+        } catch (SQLException e) {
+            logErrorFile("POST: /api/projects", e.getMessage());
+        }
+        throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "entity not found");
+    }
+
+    @DeleteMapping("/projects")
     public Boolean deleteProject(@PathVariable("id") int id) {
         try {
             DatabaseConnection dc = new DatabaseConnection();
@@ -60,10 +83,10 @@ public class ProjectsController extends DatabaseConnection implements ErrorLoggi
                 resultSet.close();
                 statement.close();
                 dc.DbConnection.close();
-                logErrorFile("/api/projects/delete/" + id, "Project not found.");
+                logErrorFile("DELETE: /api/projects/" + id, "Project not found.");
             }
         } catch (SQLException e) {
-            logErrorFile("/api/projects/delete/" + id, e.getMessage());
+            logErrorFile("DELETE: /api/projects/" + id, e.getMessage());
         }
         return false;
     }
